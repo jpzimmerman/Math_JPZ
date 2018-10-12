76d961de293b2465d73273eb53ec82847d4d313e
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-12 15:18:50 -0400

    this is my first commit for a multiplication tables program, part of DevBuild Assessment #1 (problem #2)

--- DevBuild.Math_JoshuaZimmerman/Program.cs
+++ DevBuild.Math_JoshuaZimmerman/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevBuild.Utilities;
 
 namespace DevBuild.Math_JoshuaZimmerman
 {
     class Program
     {
+        private const int MULTIPLICATION_TABLE_MAX_SIZE = 12;
+
         static void Main(string[] args)
         {
+            int userNumber = 0;
+            YesNoAnswer userAnswer = YesNoAnswer.AnswerNotGiven;
+
+            Console.Write(  "***********************************************************\n" +
+                            "*          Dev.Build(2.0) - Multiplication Tables         *\n" +
+                            "***********************************************************\n\n");
+            while (true)
+            {
+                //Prompt user to enter number, verify that input was indeed a number
+                Console.Write("Please enter a number: ");
+                while (!int.TryParse(Console.ReadLine(), out userNumber))
+                {
+                    Console.Write("Please enter a number: ");
+                }
+
+                //Send user's number to PrintMultiplicationTable for display of multiples
+                PrintMultiplicationTable(userNumber);
+
+                //ask the user if they would like to enter another number
+                userAnswer = UserInput.GetYesOrNoAnswer("Would you like to enter another number? ");
+                switch (userAnswer)
+                {
+                    case YesNoAnswer.Yes: userAnswer = YesNoAnswer.AnswerNotGiven; continue;
+                    case YesNoAnswer.No: userAnswer = YesNoAnswer.AnswerNotGiven; return;
+                }
+            }
         }
+
+        static void PrintMultiplicationTable(int factor)
+        {
+            //Print multiplication table header
+            Console.WriteLine("\n");
+            Console.WriteLine("|      {0}".PadLeft(17), factor);
+            Console.WriteLine("-------|-------");
+
+            for (int i = 0; i <= MULTIPLICATION_TABLE_MAX_SIZE; i++)
+            {
+                int multiple = factor * i;
+                Console.WriteLine(i.ToString().PadLeft(3) + "|".PadLeft(5) + multiple.ToString().PadLeft(7));
+            }
+            Console.WriteLine("\n");
+        }
+
     }
 }
---
+++ DevBuild.Math_JoshuaZimmerman/Utilities/UserInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.Utilities
+{
+    public enum YesNoAnswer { No = 0, Yes = 1, AnswerNotGiven = 2, } //for convention's sake, let's make sure Yes = 1 and No = 0;
+    
+    class UserInput
+    {
+
+        /// <summary>
+        /// This function prompts the user to type "y", "yes", "n", or "no" to provide a yes-or-no answer. Function stays in a loop until 
+        /// user enters something we recognize as a yes or no answer.
+        /// </summary>
+        /// <returns>A yes-or-no answer enum set to Yes, No, or Answer Not Given. </returns>
+        public static YesNoAnswer GetYesOrNoAnswer(string questionPrompt)
+        {
+            YesNoAnswer tmp = YesNoAnswer.AnswerNotGiven;
+            string userResponse = "";
+
+            while ( userResponse != "y" && userResponse != "yes" && 
+                    userResponse != "n" && userResponse != "no")
+            {
+                Console.Write(questionPrompt);
+                userResponse = Console.ReadLine().Trim().ToLower();
+            }
+            switch (userResponse)
+            {
+                case "y":
+                case "yes": tmp = YesNoAnswer.Yes; break;
+                case "n":
+                case "no":  tmp = YesNoAnswer.No; break;
+                default: tmp = YesNoAnswer.AnswerNotGiven; break;
+            }
+            return tmp;
+        }
+    }
+}
